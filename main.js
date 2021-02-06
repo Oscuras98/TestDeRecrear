6f631825df26db6fa64057269b90c70a83ea7214
Pirate Radio Show <75942561+Oscuras98@users.noreply.github.com>
2021-02-06 19:49:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("ENG: Welcome Hackers to the μChip Clan's Official Website!");
+console.log("ESP: Un saludo a todos los Hackers en la página official del Clan μChip!");
+console.log("HUN: Üdvözletem minden Hacker-nek a μChip Klán hivatalos weboldalán! ;-)");
+function handleSelect(elem) {
+    window.location = elem.value + "";
+}
